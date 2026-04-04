552535dd35a270398597efb8919a690c0231e0f9
Natanael <natanael_magalhaes@msn.com>
2026-04-04 02:11:10 -0300

    log

--- _plugins/venue_name.rb
+++ _plugins/venue_name.rb
@@ -3,6 +3,7 @@ module Jekyll
     def venue_name(input)
       return nil if input.nil?
       site = @context.registers[:site]
+      Jekyll.logger.debug "VenueName:", "Venue: #{input}, Data: #{site.data.fetch('venues')}"
       site.data.fetch('venues', {}).transform_keys(&:downcase).fetch(input.downcase, {}).fetch('name', input)
     end
   end
