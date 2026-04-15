e5a8afd58f572e8840ea11d3ebf8304510aa8d9d
Natanael <natanael_magalhaes@msn.com>
2026-04-15 19:34:26 -0300

    generator for publications

--- lib/jekyll-academic/venue_name.rb
+++ lib/jekyll-academic/venue_name.rb
@@ -1,9 +1,38 @@
 module Jekyll
+  module VenueNameHelper
+    def self.lookup(site, input)
+      return nil if input.nil?
+
+      site
+        .data
+        .fetch('venues', {})
+        .transform_keys(&:downcase)
+        .fetch(input.downcase, {})
+        .fetch('name', input)
+    end
+  end
+
   module VenueName
     def venue_name(input)
-      return nil if input.nil?
       site = @context.registers[:site]
-      site.data.fetch('venues', {}).transform_keys(&:downcase).fetch(input.downcase, {}).fetch('name', input)
+      VenueNameHelper.lookup(site, input)
+    end
+  end
+
+  class MetaVenueName < Generator
+    safe true
+    priority :low
+
+    def generate(site)
+      return unless site.collections.key?('publications')
+
+      site.collections['publications'].docs.each do |doc|
+        next if doc.data['description']
+        next unless doc.data['doi'] && doc.data['venue']
+
+        name = Jekyll::VenueNameHelper.lookup(site, doc.data['venue'])
+        doc.data['description'] = "Published in #{name}"
+      end
     end
   end
 end
